bb373861f81914fc9d2519e7b48e01afc5ee0bd1
sadath-12 <90619459+sadath-12@users.noreply.github.com>
2022-06-13 20:00:41 +0530

    a

--- components/Testimonials/Section1.jsx
+++ components/Testimonials/Section1.jsx
@@ -4,7 +4,7 @@ const Section1 = () => {
   return (
     <div className="flex flex-col space-y-3 w-[720px] mx-auto p-5">
     <div>
-      <p className="text-gray-700 text-[20px] ">Beauty Comes First</p>
+      <p className="text-gray-700 text-[20px] font-medium ">Beauty Comes First</p>
       <h2 className="font-extrabold text-[38px] ">WE CREATE FASHION</h2>
     </div>
     <h3 className="text-sm font-medium">
@@ -12,7 +12,7 @@ const Section1 = () => {
     </h3>
 
     <div className="w-[150px] h-[2px] bg-[#000000] " />
-    <p className="text-left text-gray-500">
+    <p className="text-left text-gray-600 text-sm font-medium tracking-wider">
       1001creators creates the perfect meeting between fashion designers and
       fashion lovers. 1001creators is a paradise for fashionistas looking
       for French and international designers clothes, strong pieces to
